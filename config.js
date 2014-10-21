b0c54e51091ad81eba2afb08ae788d68d96f8f48
Popa Adrian Marius <mapopa@reea.net>
2014-10-21 12:42:28 +0300

    add config for tests

---
+++ config.js
@@ -0,0 +1,10 @@
+
+exports.cfg = {
+  // Database connection settings
+//  db: "127.0.0.1:D:\\Work\\Den\\Node\\fb-isql\\test-db\\test.fdb",
+  db: "localhost:/test/test.fdb",
+  user: "sysdba",
+  password: "masterkey",
+  role: ""
+};
+
