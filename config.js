44e89abdfd015d53654e357d2b16cf3d4e9e8721
Popa Adrian Marius <mapopa@reea.net>
2014-10-21 15:27:01 +0300

    add test fdb correct path

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 exports.cfg = {
   // Database connection settings
 //  db: "127.0.0.1:D:\\Work\\Den\\Node\\fb-isql\\test-db\\test.fdb",
-  db: "localhost:/test/test.fdb",
+  db: "localhost:/tmp/test.fdb",
   user: "sysdba",
   password: "masterkey",
   role: ""
